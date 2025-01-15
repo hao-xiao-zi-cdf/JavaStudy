3e9bc1fa10434014f374f703548cd2172e2ecceb
hao-xiao-zi <3425588767@qq.com>
2025-01-15 16:42:33 +0800

    测试

---
+++ GitHubTest/src/Test.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 34255
+ * Date: 2025-01-15
+ * Time: 16:23
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello GitHub");
+        System.out.println("hello GitHub");
+        System.out.println("hello GitHub");
+        System.out.println("hello GitHub");
+        System.out.println("hello GitHub");
+        System.out.println("hello GitHub");
+    }
+}
