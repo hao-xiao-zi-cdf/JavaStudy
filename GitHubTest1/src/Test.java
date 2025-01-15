b69e21a2fd53b79239da9946bea6a35f48543a59
hao-xiao-zi <3425588767@qq.com>
2025-01-15 16:45:53 +0800

    代码测试提交2

---
+++ GitHubTest1/src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 34255
+ * Date: 2025-01-15
+ * Time: 16:44
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
