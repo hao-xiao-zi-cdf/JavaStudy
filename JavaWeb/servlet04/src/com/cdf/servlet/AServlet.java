d35c613ef19129e6ded7da0d1101d42a6c10f534
hao_xiao_zi <xh483817@gmail.com>
2025-01-19 19:56:04 +0800

    ServletContext接口学习(1/3)

---
+++ JavaWeb/servlet04/src/com/cdf/servlet/AServlet.java
@@ -0,0 +1,29 @@
+package com.cdf.servlet;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 34255
+ * Date: 2025-01-19
+ * Time: 19:35
+ */
+public class AServlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        //获取ServletContext对象
+        //第一种方法：通过getServletContext()方法获取
+        ServletContext application = getServletContext();
+
+        out.print(application);
+    }
+}
