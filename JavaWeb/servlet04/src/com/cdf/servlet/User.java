8cc3dda219b2b69750deadc92b3f03c08c26be89
hao_xiao_zi <xh483817@gmail.com>
2025-01-20 16:38:04 +0800

    ServletContext接口学习(3/3)

---
+++ JavaWeb/servlet04/src/com/cdf/servlet/User.java
@@ -0,0 +1,42 @@
+package com.cdf.servlet;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 34255
+ * Date: 2025-01-20
+ * Time: 16:21
+ */
+public class User {
+    public int id;
+    public String name;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public User(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
