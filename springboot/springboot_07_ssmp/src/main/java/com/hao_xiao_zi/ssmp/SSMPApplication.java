d8089a3f69eda790f1976e3e9f42ca6d3d185407
hao_xiao_zi <xh483817@gmail.com>
2025-04-06 20:17:51 +0800

    Html-day1

--- springboot/springboot_07_ssmp/src/main/java/com/hao_xiao_zi/ssmp/SSMPApplication.java
+++ springboot/springboot_07_ssmp/src/main/java/com/hao_xiao_zi/ssmp/SSMPApplication.java
@@ -1,6 +1,5 @@
 package com.hao_xiao_zi.ssmp;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
