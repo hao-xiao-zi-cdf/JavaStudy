71200cc48559a2ccdae6440db3eb24c13ede88e2
hao_xiao_zi <xh483817@gmail.com>
2025-05-04 21:07:30 +0800

    vuex

---
+++ Vue/day3-vue2入门/component/组件注册/components/WebFoot.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="foot">
+    <div>Foot</div>
+    <WebButton></WebButton>
+  </div>
+</template>
+
+
+<script setup>
+
+</script>
+
+<style scoped>
+.foot {
+  width: 300px;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #3f8932;
+}
+</style>
