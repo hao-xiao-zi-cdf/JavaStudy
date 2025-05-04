a6f380f718ddc745cffe4fa62639b3fc7a79f3a9
hao_xiao_zi <xh483817@gmail.com>
2025-05-04 20:59:05 +0800

    vuex

---
+++ Vue/day3-vue2入门/component/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="app">
+<!--    快捷链接:XtxShortCut-->
+    <XtxShortCut></XtxShortCut>
+<!--    顶部导航:XtxHeaderNav-->
+    <XtxHeaderNav></XtxHeaderNav>
+<!--    轮播区域:XtxBanner-->
+    <XtxBanner></XtxBanner>
+<!--    新鲜好物:XtxNewGoods-->
+    <XtxNewGoods></XtxNewGoods>
+<!--    热门品牌:XtxHotBrand-->
+    <XtxHotBrand></XtxHotBrand>
+<!--    最新专题:XtxTopic-->
+    <XtxTopic></XtxTopic>
+<!--    版权底部:XtxFooter-->
+    <XtxFooter></XtxFooter>
+  </div>
+</template>
+
+<script>
+//导入组件
+import XtxBanner from "./components/XtxBanner.vue";
+import XtxNewGoods from "./components/XtxNewGoods.vue";
+import XtxHotBrand from "./components/XtxHotBrand.vue";
+import XtxFooter from "./components/XtxFooter.vue";
+import XtxTopic from "./components/XtxTopic.vue";
+import XtxShortCut from "./components/XtxShortCut.vue";
+import XtxHeaderNav from "./components/XtxHeaderNav.vue";
+
+export default{
+  //注册组件
+  components:{
+    XtxShortCut,
+    XtxHeaderNav,
+    XtxBanner,
+    XtxNewGoods,
+    XtxHotBrand,
+    XtxTopic,
+    XtxFooter,
+  }
+}
+</script>
+
+<style>
+
+</style>
