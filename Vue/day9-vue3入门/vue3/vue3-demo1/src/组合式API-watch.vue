707d3ab605250a15ffdf61f36933b05c9a0b6459
hao_xiao_zi <xh483817@gmail.com>
2025-05-06 23:24:57 +0800

    vue3学习

---
+++ Vue/day9-vue3入门/vue3/vue3-demo1/src/组合式API-watch.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref, watch } from 'vue'
+// watch函数用来监听一个或多个数据的变化，当监听的数据变化时，执行watch里的回调函数
+const username = ref('张三')
+const password = ref('223781')
+const userInfo = ref({
+  age: 20,
+  sxe: '男'
+})
+const changeUserName = () => {
+  username.value = '李四'
+}
+const changePassWord = () => {
+  password.value = '123456'
+}
+const changeUserInfo = () => {
+  userInfo.value.age++
+}
+watch(
+  // 监听一个数据传入对象的ref对象
+  // username, (newVal, oldVal) => {
+  //   console.log(newVal, oldVal)
+  // }
+  // 监听多个数据传入ref数组对象
+  [username, password, userInfo],(newVal, oldVal) => {
+    console.log(newVal, oldVal)
+  },{immediate: true, deep: true}
+)
+
+// 精准监听对象中的某个属性变化
+watch(() => userInfo.value.age, () => {
+  console.log(userInfo.value.age)
+})
+</script>
+
+<template>
+<div>
+  {{ username }}
+  <button @click="changeUserName">修改名字</button>
+  <hr>
+  {{ password }}
+  <button @click="changePassWord">修改密码</button>
+  <hr>
+  {{ userInfo }}
+  <button @click="changeUserInfo">修改信息</button>
+</div>
+</template>
+
+<style scoped>
+
+</style>
